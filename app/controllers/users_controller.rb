d3232baa7c6df81676aeda033be8c4165a5c68bd
mark-drastrup <mark.drastrup@gmail.com>
2019-10-23 23:31:46 +0900

    Rendering list of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :find_user, only: [:show, :edit, :update, :destroy]
   def index
+    @users = User.all.order("created_at DESC")
   end
 
   def new
