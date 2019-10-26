54cef3bd12f24fc449bb11db07283f3ab49df7e2
mark-drastrup <mark.drastrup@gmail.com>
2019-10-26 15:16:58 +0900

    Adding basic authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :require_login, only: [:create, :edit, :update, :destroy]
   before_action :find_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all.order("created_at DESC")
@@ -43,6 +44,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :id)
   end
 end
