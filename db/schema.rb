763d8716b3d19be2dc8420f95c20a59e81b31a07
mark-drastrup <mark.drastrup@gmail.com>
2019-10-27 18:26:55 +0900

    Updating schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_125902) do
+ActiveRecord::Schema.define(version: 2019_10_27_091755) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema.define(version: 2019_10_24_125902) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
+  add_foreign_key "posts", "users"
 end
